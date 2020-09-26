c663e785712df0490260da30c665f6c5378878d3
William Cao <williamacao@gmail.com>
2020-09-26 04:49:04 +0000

    Automatically backed up by Learn

---
+++ lib/get_requester.rb
@@ -0,0 +1,19 @@
+require 'open-uri'  #loading module fo URI
+require 'net/http'  #loding module for net/http
+require 'json'      #laofind module for json
+
+class GetRequester
+  def initialize(url)
+    @url = url
+  end
+
+  def get_response_body
+    uri = URI.parse(@url)
+    response = Net::HTTP.get_response(uri)
+    response.body
+  end
+
+  def parse_json
+    JSON.parse(get_response_body)
+  end
+end
